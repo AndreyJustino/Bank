b46b683ba147185f32a779831527941836efcdbd
AndreyJustino <andreyjustino2020@gmail.com>
2024-10-04 21:05:24 -0300

    feat: add code routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
+import Login from "../pages/Login.jsx";
+import PageSendPix from "../pages/PageSendPix.jsx";
+import PayPix from "../pages/PayPix.jsx";
+import PayPixOption from "../pages/PayPixOption.jsx";
+import Register from "../pages/Register.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +15,26 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/pagesendpix",
+        element: <PageSendPix />,
+      },
+      {
+        path: "/paypix",
+        element: <PayPix />,
+      },
+      {
+        path: "/paypixoption",
+        element: <PayPixOption />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
