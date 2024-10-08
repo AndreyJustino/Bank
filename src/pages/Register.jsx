f7f05b510488b894f9241aa557b91b09b318a337
AndreyJustino <andreyjustino2020@gmail.com>
2024-10-08 15:53:32 -0300

    feat: add css global for form Register and Login

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,41 +10,43 @@ function Register() {
   }
 
   return (
-    <div className={style.containerRegister}>
-      <section className={style.sectionRegister}>
-        <div className={style.molduraPessoaInter}>
+    <div className="containerForm">
+      <section className="sectionForm">
+        
+        <div className="molduraPessoaInterRL">
           {/* <img src={foto} alt="Logo" /> */}
         </div>
-        <form autoComplete='off' onSubmit={cadastrar} className={style.formRegister}>
-          <div className={style.molduraLogoInter}>
+        
+        <form autoComplete='off' onSubmit={cadastrar} className="formRL">
+          <div className="molduraLogoInterRL">
             <img src={logo} alt="Logo inter" />
           </div>
-          <div className={style.blocoRegister}>
+          <div className="blocoRL">
             <label htmlFor="name">Nome:</label>
             <input type="text" id="name" name="name" placeholder="Nome completo..." />
           </div>
-          <div className={style.blocoRegister}>
+          <div className="blocoRL">
             <label htmlFor="email">E-mail:</label>
             <input type="email" id="email" name="email" placeholder="E-mail..."/>
           </div>
-          <div className={style.blocoRegister}>
+          <div className="blocoRL">
             <label htmlFor="email">E-mail:</label>
             <input type="email" id="email" name="email" placeholder="E-mail..."/>
           </div>
-          <div className={style.blocoRegister}>
+          <div className="blocoRL">
             <label htmlFor="nascimento">Data de nascimento:</label>
             <input type="date" id="nascimento" name="nascimento" />
           </div>
-          <div className={style.blocoRegister}>
+          <div className="blocoRL">
             <label htmlFor="telefone">Telefone:</label>
             <input type="tel" id="telefone" name="telefone" placeholder="Telefone..."/>
           </div>
-          <div className={style.blocoRegister}>
+          <div className="blocoRL">
             <label htmlFor="password">Senha:</label>
             <input type="password" id="password" name="senha" placeholder="Senha..."/>
           </div>
-          <p className={style.paragrafoLoginRegister}>Ja tem conta? <Link to="/login" className={style.linkRegister}>Clique aqui.</Link></p>
-          <button type="submit" className={style.buttonRegister}>Cadastrar</button>
+          <p className="paragrafoRL">Ja tem conta? <Link to="/login" className="linkRL">Clique aqui.</Link></p>
+          <button type="submit" className="buttonRL">Cadastrar</button>
         </form>
       </section>
 
