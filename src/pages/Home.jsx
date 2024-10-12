dba65fa1facdc61139073f3e09ff9ed6f62b85b6
JuandaConceicao <juancssl15@gmail.com>
2024-10-12 13:05:50 -0300

    feat: add new functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
 import style from "./Home.module.css"
 import NavbarSuperior from '../components/NavbarSuperio.jsx';
 import SaldoArea from '../components/SaldoArea.jsx';
@@ -7,6 +9,17 @@ import Promocao from '../components/Promocao.jsx';
 import NavbarInferior from '../components/NavbarInferior.jsx';
 
 function Home() {
+
+  const navigate = useNavigate()
+
+  useEffect(() =>{
+    const auth = Cookies.get("auth")
+
+    if(!auth){
+      return navigate("/login")
+    }
+  },[])
+  
   return (
     <div className={style.headerHome}>
       <NavbarSuperior />
