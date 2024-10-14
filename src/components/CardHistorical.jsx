0f5754469ab25726b21e08993934b056cf147ae3
AndreyJustino <andreyjustino2020@gmail.com>
2024-10-14 17:04:12 -0300

    feat: tratando data

--- src/components/CardHistorical.jsx
+++ src/components/CardHistorical.jsx
@@ -3,11 +3,14 @@ import style from "./CardHistorical.module.css";
 import iconNext from "../assets/icon/iconsNext.png";
 
 function CardHistorical({ valor, data, id }) {
+  const date = new Date(data)
+  const formattedDate = date.toLocaleDateString('pt-BR');
+  
   return (
     <div className={style.cardFavorite}>
       <div>
-        <h1>{valor}</h1>
-        <p>{data}</p>
+        <h1>R$ {valor}</h1>
+        <p>{formattedDate}</p>
         <p>{id}</p>
       </div>
       <img src={iconNext} alt="icon next" />
